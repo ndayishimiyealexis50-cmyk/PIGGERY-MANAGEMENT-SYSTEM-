eb43c4ec53e4d9209de0272f222461afb2d989aa
ndayishimiyealexis50-cmyk <ndayishimiyealexis50@gmail.com>
2026-04-16 11:48:23 +0200

    Add files via upload

--- piggery-react/src/main.jsx
+++ piggery-react/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+// Replaces: §28 APP RENDER  (root.render / requestAnimationFrame splash dismiss)
+// in index.html.
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
