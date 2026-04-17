094c11ae009b9fe7dcbd45b73a6af92ef870de5a
ndayishimiyealexis50-cmyk <ndayishimiyealexis50@gmail.com>
2026-04-17 14:34:58 +0200

    Update PigAssessmentHistory.jsx

--- piggery-react/src/pages/PigAssessmentHistory.jsx
+++ piggery-react/src/pages/PigAssessmentHistory.jsx
@@ -21,7 +21,7 @@
 
 import { useState } from 'react';
 import { C, S } from '../utils/constants';
-import { fsSet, getOnlineFarmData, setOnlineFarmData } from '../lib/firestore';
+import { fsSet, getOnlineFarmData, setOnlineFarmData } from '../utils/storage';
 import { toDay } from '../utils/helpers';
 import Toast from '../components/Toast';
 
